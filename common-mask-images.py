0cf1b9a280d0340d9a13d719991041234c5de8d0
James Maze <jmaze@uoregon.edu>
2024-11-07 13:13:34 -0800

    time for a run

--- common-mask-images.py
+++ common-mask-images.py
@@ -1,15 +1,15 @@
-# %% 
+# %% 1.0 Libraries
+import pprint as pp
+
 import numpy as np
 import rasterio as rio
 from rasterio.windows import from_bounds
 import rioxarray as rxr
 import geopandas as gpd
 
-# %% 1. Mask the LandSat and Sentinel2 Images 
-# B) ?? Resample the Sentinel-2 image to 30 meter resolution
-# C) Apply the common mask to both images
-# D) Write to file
+# %% 2. Use common cloud mask on the LandSat and Sentinel2 Images 
 
+band_color = 'green'
 
 ls_green_band = 2 # Green is the 2nd band in export
 s2_green_band = 2 # Green is the 2nd band in the export, even though its Band#3 in Sentinel-2
@@ -38,12 +38,37 @@ with rio.open('./data/sentinel2-images/sentinel2-images-merged.tif') as s2_img_s
     print(s2_data.shape)
     print(ls_data.shape)
 
-# with rio.open('./data/common_mask.tif') as cmask_src:
-#     cmask = cmask_src.read(1)
-#     cmask_meta = cmask.meta
+with rio.open('./data/common_mask.tif') as cmask_src:
+    window_cmask = from_bounds(*intersection_bounds, cmask_src.transform)
+    cmask = cmask_src.read(1, window=window_cmask)
+    cmask_meta = cmask_src.meta
+
+    print(cmask.shape)
+
+s2_data = np.where(cmask != 1, s2_data, np.nan)
+ls_data = np.where(cmask != 1, ls_data, np.nan)
+negative_ls_pix = np.sum(ls_data < 0) / ls_data.size * 100
+print(f'Negative Landsat pixels: {negative_ls_pix:.2f}%')
+# Convert negative Landsat values to 0
+#ls_data = np.where(ls_data < 0, 0, np.nan)
+
+# Save the masked images
+out_meta = s2_meta.copy()
+out_meta.update({
+    'count': 2,
+    'dtype': 'float64',
+    'height': s2_data.shape[0],
+    'width': s2_data.shape[1]
+})
+
+with rio.open(f'./data/masked_images/{band_color}-masked.tif', 'w', **out_meta) as dst:
+    dst.write(ls_data, 1)
+    dst.set_band_description(1, f'landsat-{band_color}')
+    dst.write(s2_data, 2)
+    dst.set_band_description(2, f'sentinel2-{band_color}')
 
 
 
 
 
-# %%
+
--- merge-masks-and-landsat.py
+++ merge-masks-and-landsat.py
@@ -1,35 +1,47 @@
+# 1.0 Libraries and directories
+
 import glob
 import rasterio as rio
 from rasterio.merge import merge
 
-#%%
-# 
 ls_data_dir = './data/landsat-images'
 ls_data = glob.glob(ls_data_dir + '/*.tif')
 
 s2_masks_dir = './data/sentinel2-masks'
-s2_masks = glob.glob(s2_masks_dir + '/*.tif')
+s2_masks = glob.glob(s2_masks_dir + '/s2-mask-t*.tif')
 
-# %%
+# %% 2.0 Merge Landsat images
 
-rasters = []
+def merge_raster_list(raster_list, dataset):
 
-for p in ls_data:
-    print(p)
-    r = rio.open(p)
-    rasters.append(r)
+    rasters = []
 
-merged, out_trans = merge(rasters)
-out_meta = r.meta.copy()
-out_meta.update({
-    "driver": "GTiff",
-    "height": merged.shape[1],
-    "width": merged.shape[2],
-    "transform": out_trans,
-    'crs': r.crs
-})
+    for p in raster_list:
+        print(p)
+        r = rio.open(p)
+        rasters.append(r)
 
-output_path = './data/landsat-images/landsat-images-merged.tif'
-with rio.open(output_path, "w", **out_meta) as dest:
-    dest.write(merged)
+    merged, out_trans = merge(rasters)
+    out_meta = r.meta.copy()
+    out_meta.update({
+        "driver": "GTiff",
+        "height": merged.shape[1],
+        "width": merged.shape[2],
+        "transform": out_trans,
+        'crs': r.crs
+    })
+
+    output_path = f'./data/{dataset}/{dataset}-merged.tif'
+    with rio.open(output_path, "w", **out_meta) as dest:
+        dest.write(merged)
+
+    print('Merged raster saved to:', output_path)
+
+    return
+
+#merge_raster_list(ls_data, 'landsat-images')
+merge_raster_list(s2_masks, 'sentinel2-masks-v2')
+
+
+# %% 3.0 Merge Sentinel-2 masks
 
