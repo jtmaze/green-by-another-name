8eac30078e0e615dc031fc6993b00298d10f64f0
jtmaze <jmaze@uoregon.edu>
2024-11-15 22:45:57 -0800

    Going to be doing alot of pixel regressions, might as well make functions

---
+++ pixel-regressions-functions.py
@@ -0,0 +1,13 @@
+
+
+import random
+import rasterio as rio
+from rasterio.windows import from_bounds
+import numpy as np
+import pandas as pd
+
+from scipy import stats
+import matplotlib.pyplot as plt
+
+random.seed(20)
+
