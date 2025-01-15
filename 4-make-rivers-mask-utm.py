Ready to merge with main branch. Fixed river masking functions and PLD writing

4-make-rivers-mask-utm.py
@@ -13,32 +13,44 @@ import skimage as ski
 from skimage.morphology import binary_dilation
 
 roi_name = 'YKF_sub1'
-out_res = 30 # ensure this matches the resolution of your analysis
-dilation_dist = 150 #meters
+out_res = 30 # ensure this matches the resolution of your analysis (e.g. 30m or 60m)
+buffer_dist = 150 # The distance in meters to dilate/buffer the rivers
 
 roi_prefix = roi_name.split('_')[0]
 rivers_dir = f'./data/river_files/{roi_prefix}_river.shp'
 roi_dir = f'./data/roi_shapes/{roi_name}_shape.shp'
 out_dir = f'./data/river_files/'
+out_path = f'{out_dir}/{roi_name}_binary_rivers_dilated{buffer_dist}.tif'
+
 rivers = gpd.read_file(rivers_dir)
 roi = gpd.read_file(roi_dir)
 
 # %% 2.0 Generate a mask image from the River shapefiles
 
-def make_river_mask(roi: gpd.GeoDataFrame, rivers: gpd.GeoDataFrame, out_res: int):
+def clip_dilate_rivers(rivers: gpd.GeoDataFrame, roi: gpd.GeoDataFrame, buffer_dist: int):
+    """"""
+    # Conversion to ROI's local UTM zone
+    roi_est_utm = roi.estimate_utm_crs()
+    roi_utm = roi.to_crs(roi_est_utm)
+    rivers_utm = rivers.to_crs(roi_est_utm)
+    # Clip the rivers to roi and buffer thier shapes
+    clipped_rivers = gpd.clip(rivers_utm, roi_utm)
+    dilated_rivers = clipped_rivers.buffer(buffer_dist)
+    # Bounds for the ROI to use in writing rivers raster
+    roi_bounds = roi_utm.geometry.total_bounds
+
+    return dilated_rivers, roi_bounds
+
+def make_river_mask(bounds: tuple, 
+                    rivers_dilated: gpd.GeoDataFrame, 
+                    out_res: int,
+                    out_path: str):
     """
     Makes a binary mask from rivers shapefile and clips the mask to the roi
     """
-    est_utm = roi.estimate_utm_crs()
-    roi = roi.to_crs(est_utm)
-    rivers = rivers.to_crs(est_utm)
-    clipped_rivers = gpd.clip(rivers, roi)
-
-    bounds = roi.geometry.total_bounds # Tuple with bounding box
-    print(bounds)
+    # Get metadata from the roi's bounds (local UTM)
     width = int((bounds[2] - bounds[0]) / out_res)
     height = int((bounds[3] - bounds [1]) / out_res)
-
     transform = from_bounds(
         bounds[0], bounds[1],
         bounds[2], bounds[3],
@@ -52,13 +64,13 @@ def make_river_mask(roi: gpd.GeoDataFrame, rivers: gpd.GeoDataFrame, out_res: in
         'width': width,
         'height': height,
         'count': 1,
-        'crs': est_utm,
+        'crs': rivers_dilated.crs, #crs from dataframe (local UTM)
         'transform': transform,
         'compress': 'lzw'
     }
 
     mask = features.rasterize(
-        shapes=clipped_rivers.geometry,
+        shapes=rivers_dilated.geometry,
         out_shape=(height, width),
         transform=transform,
         fill=0,
@@ -66,46 +78,14 @@ def make_river_mask(roi: gpd.GeoDataFrame, rivers: gpd.GeoDataFrame, out_res: in
         dtype='uint8'
     )
 
-    return mask, meta
+    with rio.open(out_path, 'w', **meta) as dst:
+        dst.write(mask, indexes=1)
 
-def dilate_rivers(mask: np.array, 
-                  meta: dict, 
-                  dilation_dist: int, 
-                  out_res: int):
-    """
-    Dilates a the river mask by a specified amount in meters.
-    """
-    buffer_amnt_pixels = int(dilation_dist / out_res)
-    structuring_element = np.ones((buffer_amnt_pixels, buffer_amnt_pixels), dtype=bool)
-    pp.pp(structuring_element)
-    dilated = binary_dilation(mask, footprint=structuring_element)
-
-    return dilated
-
-def write(mask: np.array, meta: dict, out_path: str):
-    """
-    Writes binary mask to out_dir
-    """
-    if mask.dtype == bool:
-        mask = mask.astype('uint8')
-    if mask.ndim == 2:
-        mask = np.expand_dims(mask, axis=0)
-
-    
-    final_meta = meta.copy()
-
-    pp.pp(meta)
-
-    with rio.open(out_path, 'w', **final_meta) as dst:
-        dst.write(mask)
-
-
+    print(f'Processed {out_path}')
 
 # %% 3.0 Run the functions
 
-out_path = f'{out_dir}/{roi_name}_binary_rivers_dilated{dilation_dist}_UTMretest.tif'
-mask, meta = make_river_mask(roi, rivers, out_res=out_res)
-dilated_mask = dilate_rivers(mask, meta, dilation_dist=dilation_dist, out_res=out_res)
-write(mask=dilated_mask, meta=meta, out_path=out_path)
+rivers_dilated, bounds = clip_dilate_rivers(rivers, roi, buffer_dist)
+make_river_mask(bounds, rivers_dilated, out_res, out_path)
 
 # %%

5-apply-river-masks-name-bands.py
@@ -6,44 +6,33 @@ import rasterio as rio
 from rasterio.windows import from_bounds
 from rasterio.warp import calculate_default_transform, reproject, Resampling
 
-out_dir = './data/image_downloads_masked/'
-img_dir = './data/image_downloads/*.tif'
-img_list = glob.glob(img_dir)
+level = 'sr' #level should be 'sr' or 'toa'
 roi_name = 'YKF_sub1'
-roi_path = f'./data/roi_shapes/{roi_name}_shape.shp'
-roi = gpd.read_file(roi_path)
-est_utm = roi.estimate_utm_crs()
-print(est_utm)
+# Dictionary to add description to bands based on the
+band_desc = {
+    1: 'Blue',
+    2: 'Green',
+    3: 'Red',
+    4: 'NIR'
+}
 
-river_path = f'./data/river_files/{roi_name}_binary_rivers_dilated150_UTMretest.tif'
+out_dir = f'./data/{level}_images/'
+river_dir = './data/river_files/'
+img_download_dir = './data/sr_images_downloads/*.tif'
+img_list = glob.glob(img_download_dir)
+
+river_mask_path = f'{river_dir}/{roi_name}_binary_rivers_dilated150_UTMretest.tif'
+
 
 # %% 2.0 Function to mask rivers from images
 
-def reproj_img_to_utm(src: rio.DatasetReader, utm_crs: str):
-    transform, width, height = calculate_default_transform(
-        src.crs,
-        utm_crs,
-        src.width,
-        src.height,
-        *src.bounds
-    )
-    out_array = np.empty((src.count, height, width), dtype=src.dtype)
-    reproject(
-        source=src.read(),
-        destination=out_array,
-        src_transform=src.transform,
-        src_crs=src.crs,
-        dst_transform=transform,
-        dst_crs=utm_crs,
-        resampling=Resampling.nearest
-    )
-
-def apply_river_mask(img_path: str, river_path: str, out_dir: str):
+def apply_river_mask(img_path: str, river_path: str, out_dir: str, band_descript: dict):
     """
     Applies the river mask to all the bands in the images.
     Any pixel where the mask is 1 will become nodata in the output.
     """
-    file_name = img_path.split('image_downloads')[1]
+    # Only selects the file name ignores rest of path with directory
+    file_name = img_path.split('images_downloads')[1] 
     out_path = out_dir + file_name
 
     with rio.open(img_path) as src, rio.open(river_path) as mask:
@@ -69,11 +58,21 @@ def apply_river_mask(img_path: str, river_path: str, out_dir: str):
         window_img = from_bounds(*intersection_bounds, src.transform)
         window_mask = from_bounds(*intersection_bounds, mask.transform)
 
+        # Apply the mask to each band
         band_count = img_meta['count']
+        print(band_count)
         for i in range(1, band_count + 1):
             img_data = src.read(i, window=window_img)
-            mask_data = mask.read(1, window=window_mask)
 
+            # Without resampling, there's a slight mismatch between the image and mask dimensions. 
+            mask_data = mask.read(
+                1, 
+                window=window_mask,
+                out_shape=img_data.shape,
+                resampling=Resampling.nearest
+            )
+
+            # For first band mode should be write
             if i == 1:
                 mode = 'w'
                 out_meta.update({
@@ -81,20 +80,23 @@ def apply_river_mask(img_path: str, river_path: str, out_dir: str):
                     'width': img_data.shape[1],
                     'transform': src.window_transform(window_img)
                 })
+            # Use the same file for subsequent bands
             else:
                 mode = 'r+'
 
-            # Apply the mask
+            # Apply the river mask to the data
             out_img = np.where(mask_data == 1, out_meta['nodata'], img_data)
+            print(out_img.shape)
 
             # Write the output
             with rio.open(out_path, mode, **out_meta) as dst:
                 dst.write(out_img, i)
-                dst.set_band_description(i, f'Band {i}')
-
+                dst.set_band_description(i, f'{band_descript[i]}')
+                
+        print(out_img.shape)
         print(f'Processed {out_path}')
 
 # %% 3.0 Apply the river masking function
-
 for img in img_list:
-    apply_river_mask(img, river_path, out_dir)
+    apply_river_mask(img, river_mask_path, out_dir, band_desc)
+# %%