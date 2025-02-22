9945d7f074a874b2390452833e90d9fba50b6c4a
James Maze <jmaze@uoregon.edu>
2025-02-22 14:17:21 -0800

    Decent headway

--- fix_split_imgs.py
+++ fix_split_imgs.py
@@ -2,72 +2,100 @@
 
 import glob
 import re
+import numpy as np
+
 import rasterio as rio
 from rasterio.merge import merge
 from rasterio.coords import BoundingBox
-from shapely.geometry import box
-from functools import reduce
+from rasterio.warp import reproject, Resampling
 
 import pprint as pp
 
-data_dir = './data/new_test_v2/'
+data_dir = './data/new_test_v3/'
+river_dir = './data/river_files/'
+river_path = f'{river_dir}/YKF_sub3_binary_rivers_dilated180.tif'
 
 s2_files = glob.glob(f'{data_dir}/Sentinel2*.tif')
 print(s2_files)
+ls8_files = glob.glob(f'{data_dir}/Landsat8*.tif')
+print(ls8_files)
 
-batch = s2_files
+sat = 'ls8'
+if sat == 's2':
+    batch = s2_files
+else:
+    batch = ls8_files
 
 # %% 2.0 
 
-def get_full_bounds(bounds_list: list):
+# def get_full_bounds(bounds_list: list):
 
-    left = min(bb.left for bb in bounds_list)
-    right = max(bb.right for bb in bounds_list)
-    bottom = min(bb.bottom for bb in bounds_list)
-    top = max(bb.top for bb in bounds_list)
+#     left = min(bb.left for bb in bounds_list)
+#     right = max(bb.right for bb in bounds_list)
+#     bottom = min(bb.bottom for bb in bounds_list)
+#     top = max(bb.top for bb in bounds_list)
 
-    full_bounds = BoundingBox(left=left, right=right, bottom=bottom, top=top)
+#     full_bounds = BoundingBox(left=left, right=right, bottom=bottom, top=top)
 
-    return full_bounds
+#     return full_bounds
+
+
+######################################################
+
+
+ref_raster = rio.open(river_path)
+ref_trans = ref_raster.transform
+ref_crs = ref_raster.crs
+ref_bounds = ref_raster.bounds
+
 
 src_files = []
+dst_raster_list = []
 
-for path in batch:
-    
-    src = rio.open(path)
-    src_files.append(src)
-    print(src.profile['nodata'])
-    bounds_list = [src.bounds for src in src_files]
+for idx, path in enumerate(batch):
+    print(path)
+    with rio.open(path) as src:
+        # Create destination array with the shape (# bands, height, width)
+        src_data = src.read()
+        dst_data = np.empty((src.count, ref_raster.height, ref_raster.width), dtype=np.float32)
+        reproject(
+            source=src_data,
+            destination=dst_data,
+            src_transform=src.transform,
+            src_crs=src.crs,
+            dst_transform=ref_trans,
+            dst_crs=ref_crs,
+            resampling=Resampling.nearest
+        )
+        out_meta = ref_raster.meta.copy()
+        out_meta.update({
+            "driver": "GTiff",
+            "count": src.count,
+            "height": dst_data.shape[1],
+            "width": dst_data.shape[2],
+            "transform": ref_trans,
+            "dtype": 'float32',
+            "crs": ref_crs,
+            "bounds": ref_bounds
+        })
 
-
-full_bounds = get_full_bounds(bounds_list)
-pp.pp(full_bounds)
-
-merged, out_transform = merge(
-    src_files, 
-    bounds=full_bounds,
-    method='max',
-    nodata=None,
-    masked=True
-)
-
-pp.pp(out_transform)
-
-out_meta = src_files[2].meta.copy()
-out_meta.update({
-    "driver": "GTiff",
-    "height": merged.shape[1],
-    "width": merged.shape[2],
-    "transform": out_transform,
-    "crs": src_files[0].crs
-})
-
-out_path = f'{data_dir}/merged_s2_test.tif'
-
-with rio.open(out_path, 'w', **out_meta) as dest:
-    dest.write(merged)
-
-for src in src_files:
-    src.close()
+        out_path = f'{data_dir}/reproj_{sat}_test_{idx}.tif'
+        dst_raster_list.append(dst_data)
+    # with rio.open(out_path, 'w', **out_meta) as dst:
+    #     dst.write(dst_data, indexes=list(range(1, dst_data.shape[0] + 1)))
 
 # %%
+
+stacked_rasters = np.stack(dst_raster_list)
+print(stacked_rasters.shape)
+imgs_mean = np.nanmean(stacked_rasters, axis=0)
+print(imgs_mean.shape)
+mean_meta = out_meta.copy()
+mean_out_path = f'{data_dir}/mean_{sat}_composite.tif'
+with rio.open(mean_out_path, 'w', **mean_meta) as dst:
+    dst.write(imgs_mean, indexes=list(range(1, imgs_mean.shape[0] + 1)))
+
+print(f"Mean composite saved to: {mean_out_path}")
+
+# %%
+
